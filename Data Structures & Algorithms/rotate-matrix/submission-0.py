b75055d74e04ae86fb8363e1638a4c56ba6c9554
Billy Nguyen <bnvinh0808@gmail.com>
2026-06-07 13:58:44 -0700

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # [[1,2],
+        #  [3,4]]
+        n = len(matrix)
+        for layer in range(n // 2):
+            first, last = layer, n - 1 - layer
+
+            for i in range(first, last):
+                offset = i - first
+
+                # store top-left 
+                temp = matrix[first][offset]
+
+                # top-left = bottom-left
+                matrix[first][offset] = matrix[last - offset][first]
+
+                # bottom-left = bottom-right
+                matrix[last - offset][first] = matrix[last][last - offset]
+
+                # bottom-right = top-right
+                matrix[last][last-offset] = matrix[offset][last]
+
+                # top-right = (which is temp) bottom-left
+                matrix[offset][last] = temp
+
+
+
+
+     
