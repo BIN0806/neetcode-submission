5c50423d80c628a4532f72eb1d863437c59c836f
Billy Nguyen <bnvinh0808@gmail.com>
2026-06-08 22:48:21 -0700

    Add: set-zeroes-in-matrix - submission-5

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-5.py
@@ -0,0 +1,64 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows, cols = len(matrix), len(matrix[0])
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if matrix[r][c] == 0:
+        #             for new_c in range(cols):
+        #                 matrix[r][new_c] = 0
+        #             for new_r in range(rows):
+        #                 matrix[new_r][c] = 0
+        #             break
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = matrix[0][c] = 0
+
+        #             print("DEBUG:")
+        #             for row in matrix:
+        #                 print(row)
+        #             print("~")
+        # print("POST MATRIX")
+        # for row in matrix:
+        #     print(row)
+        # print("~")
+
+        for r in range(rows):
+            for c in range(cols):
+                # print(matrix[0][c])
+                # print(matrix[r][0])
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    # print(matrix)
+                    # print(f"({r,c}) -> 0")
+                    matrix[r][c] = 0 
+    
+
+
+
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if matrix[r][c] == 0:
+        #             matrix[0][c] = matrix[r][0] = 0
+        #             print("DEBUG:")
+        #             for row in matrix:
+        #                 print(row)
+        #             print("~")
+
+        # for r in range(rows):
+        #     if matrix[r][0] == 0:
+        #         for c in range(cols):
+        #             matrix[r][c] = 0 
+    
+        # for c in range(cols):    
+        #     if matrix[0][c] == 0:
+        #         for r in range(rows):
+        #             matrix[r][c] = 0 
+
+
+        # 1 0 3
+        # 0 0 1 
+        # 1 1 1
+
+
+        
