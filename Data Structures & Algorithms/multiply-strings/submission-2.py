28af53a2ee8431c8d729047fc09b44f595958816
Billy Nguyen <bnvinh0808@gmail.com>
2026-06-10 06:45:31 -0700

    Add: multiply-strings - submission-2

---
+++ Data Structures & Algorithms/multiply-strings/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # "3"
+        # "4" 
+        # "12"
+
+
+        # "5"
+        # "10"
+        # 50
+
+        #   12 
+        # x 12
+        # ----
+        #   24
+        # +120
+        # ----
+        #  144
+        
+        #   99
+        # x 99
+        # ----
+        # 9801
+
+        # O(m*n)
+        # O(m + n)
+        n1, n2 = len(num1), len(num2)
+        result = [0] * (n1 + n2)
+
+        num1, num2 = num1[::-1], num2[::-1]
+
+        for i1 in range(n1):
+            for i2 in range(n2):
+                product = int(num1[i1]) * int(num2[i2])
+
+                result[i1+i2] += product
+                result[i1+i2+1] += result[i1+i2] // 10
+                result[i1+i2] = result[i1+i2] % 10
+        print("POST", result)
+        result = result[::-1]
+        print("REVERSE", result)
+        begin = 0
+        n = len(result)
+        while begin < n and result[begin] == 0 :
+            begin += 1
+        print("BEGIN", begin)
+        result = result[begin:]
+        print("RESULT")
+
+        result = map(str, result)
+
+        return "".join(result)
