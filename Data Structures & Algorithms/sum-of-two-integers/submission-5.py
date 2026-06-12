316c8a14ceaf912ba5dfaa5f4f3e4cd960edf85b
Billy Nguyen <bnvinh0808@gmail.com>
2026-06-12 13:57:33 -0700

    Add: sum-of-two-integers - submission-5

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        res = 0
+        carry = 0
+
+        # Python integers can be represnted as negative so this handles itself (-4) + (3) = 3 - 4
+        for i in range(32):
+            # gets the ith bit
+            a_i, b_i = (a >> i) & 1, (b >> i) & 1
+
+            # what is the cur_bit, includes carry if were prev incremented
+            cur_bit = a_i ^ b_i ^ carry
+
+            # do we have enough for a carry in our cur_bit?
+            carry = (a_i + b_i + carry) >= 2
+
+            if cur_bit:
+                res |= (1 << i) # set the ith digit to 1, else leave as 0 
+        
+
+
+        # If Negative => Invert all bits
+        mask = 0xFFFFFFFF
+        if res > 0x7FFFFFFF:
+            # Python has abritary precrsion, limit to 32 bits
+            # Doing res XOR Mask gets the Magnitude
+            # ~(x) == -x - 1 
+            res = ~(res ^ mask) # == (-(res^mask) - 1)
+            
+        if res & 0x80000000:
+            res -= 0x10000000
+
+        return res
