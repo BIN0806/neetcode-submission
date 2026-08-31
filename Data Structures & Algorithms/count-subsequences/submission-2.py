c31cd16f3a7db454cdd1247fcce671e46f3f9572
Billy Nguyen <bnvinh0808@gmail.com>
2026-08-30 22:35:54 -0700

    Add: count-subsequences - submission-2

---
+++ Data Structures & Algorithms/count-subsequences/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        l1, l2 = len(s), len(t)
+        # let dp[i] = the number of subsequences that larger_str[:i], smaller_str[:i] have 
+        # let dp[i] = dp[i-1] + 
+        if not l1 or not l2: return 0 
+
+        dp = [[0 for _ in range(l2)] for _ in range(l1)]
+        for i in range(1, l1):
+            dp[i][0] = dp[i-1][0] + (1 if s[i] == t[0] else 0)
+
+        # print("pre")
+        # for row in dp:
+        #     print(row)
+
+        for r in range(1, l1):
+            for c in range(1, l2):
+                dp[r][c] = dp[r-1][c] + (dp[r-1][c-1] if s[r] == t[c] else 0)
+        
+        # print("pos")
+        # for row in dp:
+        #     print(row)
+
+        return dp[-1][-1]
