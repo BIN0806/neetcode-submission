095a10d72c5f91cdefb82e3da21c367ac7185036
Billy Nguyen <bnvinh0808@gmail.com>
2026-08-30 17:02:18 -0700

    Add: longest-increasing-path-in-matrix - submission-21

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-21.py
@@ -0,0 +1,99 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # n, m = len(matrix), len(matrix[0])
+        # dp = [ [1] * m for _ in range(n)]
+        # parent = [ [ None for _ in range(m)] for _ in range(n)]
+
+        # from collections import deque 
+        # q = deque()
+
+        # def out_of_bounds(r, c):
+        #     if r >= n or c >= m or r < 0 or c < 0:
+        #         return True
+
+        # def update(r, c):
+        #     DIR = [(-1, 0), (0, -1), (0, 1), (1,0)]
+        #     original = matrix[r][c]
+
+        #     for dx, dy in DIR:
+        #         if out_of_bounds(r+dx, c+dy): 
+        #             continue
+
+        #         directional_value = matrix[r+dx][c+dy]
+
+        #         if original < directional_value and 1 + dp[r][c] > dp[r+dx][c + dy]:
+        #             dp[r+dx][c+dy] = 1 + dp[r][c]
+        #             q.append((r+dx, c+dy))
+        #             parent[r+dx][c+dy] = (r, c)
+        # nums = []
+        # for r in range(n):
+        #     for c in range(m):
+        #         nums.append((r, c))
+        # q = deque(sorted(nums, key=lambda x: matrix[x[0]][x[1]]))
+
+        # while q:
+        #     r, c = q.popleft()
+        #     update(r, c)
+
+        # best = max( ((r,c) for r in range(n) for c in range(m)), key=lambda x: dp[x[0]][x[1]])
+        
+        # path = []
+        # cur = best
+        # while cur:
+        #     path.append(matrix[cur[0]][cur[1]])
+        #     cur = parent[cur[0]][cur[1]]
+            
+        # print(f"DEBUG:")
+        # for r in dp:
+        #     print(r)
+
+        # path.reverse()
+        # print(path)
+
+
+        # return dp[best[0]][best[1]]
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = {}     
+        DIR = [(-1,0),(1,0),(0,-1),(0,1)]
+        def out_of_bounds(r, c):
+            if r >= ROWS or c >= COLS or r < 0 or c < 0:
+                return True
+
+        def dfs(r, c, prev_val):
+            if out_of_bounds(r, c) or prev_val >= matrix[r][c]:
+                return -1
+
+            if (r,c) in dp:
+                return dp[(r, c)][0]
+                # because if we seen the cell already it has done it's best, defintition of dp
+                # to make it more clear we have solved the longest lenght it has donw alreayd?
+
+            res = 1
+            for dx, dy in DIR:
+                nr, nc = r+dx, c+dy
+                path_len = 1 + dfs(nr, nc, matrix[r][c])
+                if path_len > res:
+                    res = path_len
+                    dp[(r, c)] = (res, (nr, nc))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, -1) # each cell is len 1
+
+        start_cell = max(dp.values(), key=lambda x: x[0])
+        # print(f"DEBUG:")
+        # for r in dp:
+        #     print(r)
+        
+        # cur = start_cell[1]
+        # path = []
+        # while cur is not None:
+        #     x, y = cur[0], cur[1]
+        #     path.append(matrix[x][y])
+        #     cur = dp[(x, y)][1]
+        # print(path)
+        
+        return start_cell[0]
+
