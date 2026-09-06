6ae2b6bc461b9c8fea9bfdec7cc0aa2af2206bf8
Billy Nguyen <bnvinh0808@gmail.com>
2026-09-05 22:49:44 -0700

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        map = {} # num -> cur_size of previous values 
+
+        for num in nums:
+            if num-1 in map: 
+                map[num] = 1 + map[num-1]
+            else:
+                map[num] = 1
+        print(map)
+        return max(map.values())
