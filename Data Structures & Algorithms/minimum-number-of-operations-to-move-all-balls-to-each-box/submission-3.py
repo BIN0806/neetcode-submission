b8f347ce88829e5d09478839754b25c124645033
Billy Nguyen <bnvinh0808@gmail.com>
2026-09-06 14:42:25 -0700

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-3

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        if n == 0: return [] 
+
+        box = [int(digit) for digit in boxes]
+
+        print(box)
+        suffix = [0] * n
+        for i in range(n-2, -1, -1):
+            suffix[i] = suffix[i + 1] + box[i + 1]
+        print("SUFFIX", suffix)
+
+        prefix = [0] * n
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + box[i - 1]
+        print("PREFIX", prefix)
+
+        res = []
+        for i in range(n):
+            count = 0
+            for j in range(i):
+                count += prefix[j] 
+            for j in range(n-1, i, -1):
+                count += suffix[j]
+            res.append(count)
+        
+        return res
