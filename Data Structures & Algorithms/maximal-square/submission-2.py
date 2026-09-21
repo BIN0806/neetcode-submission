e2dab8af77fb4b5563f29dc4f8554e8261264a8c
Billy Nguyen <bnvinh0808@gmail.com>
2026-09-20 23:18:16 -0700

    Add: maximal-square - submission-2

---
+++ Data Structures & Algorithms/maximal-square/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        
+
+        # dp[i][j] = 1 ** 2 (if matrix[i][j] == "1")
+        #          + (x - i) ** 2 (if matrix[i:x][j] == "1")
+        #          = max area of 1's 
+
+
+        N, M = len(matrix), len(matrix[0])
+        dp = [[0] * M for _ in range(N)]
+
+        for i in range(N):
+            dp[i][0] = int(matrix[i][0])
+        for j in range(M):
+            dp[0][j] = int(matrix[0][j])
+
+        for i in range(1, N):
+            for j in range(1, M):
+                if int(matrix[i][j]):
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + int(matrix[i][j])
+
+        
+        return max(max(row) for row in dp) ** 2
